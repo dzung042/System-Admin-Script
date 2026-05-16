87f197d4c56d6518bab758e7f85c47cf7966f4b3
dzung042 <dzungbk.042@gmail.com>
2026-05-16 12:07:25 +0700

    Add certbot-auth.sh for DNS challenge automation

---
+++ dnsbkns/certbot-auth.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+BKNS_API="https://my.bkns.net/api"
+
+source /root/.secrets/bkns-dns.env
+
+BASIC_TOKEN=$(printf "%s:%s" "$USERNAME" "$PASSWORD" | base64 -w0)
+
+POST_DATA="{
+    \"name\": \"_acme-challenge.${CERTBOT_DOMAIN}\",
+    \"ttl\": 60,
+    \"type\": \"TXT\",
+    \"content\": \"${CERTBOT_VALIDATION}\"
+}"
+
+echo "$POST_DATA"
+
+curl -v -X POST \
+"${BKNS_API}/service/${SERVICE_ID}/dns/${ZONE_ID}/records" \
+-H "Authorization: Basic ${BASIC_TOKEN}" \
+-H "Content-Type: application/json" \
+-d "${POST_DATA}"
+
+sleep 30
